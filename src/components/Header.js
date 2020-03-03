579f799b3fa27acc19e2c77312c0087a044a33bd
Video Stream <videostream@192.168.2.110>
2020-03-03 01:59:01 -0700

    New header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 export default class Header extends React.Component {
     render(){
         return (
             <div className = "header">
-                <h1>This is the beginning of Cytoscape project</h1>
+            <FontAwesomeIcon icon={faCoffee} color = "#4BA9EA" size = "4x" className = "icon"/>
+            <i className="fab fa-twitter"></i>
+                <h1 style={{color: "#4BA9EA"}}>This is the beginning of Cytoscape project</h1>
             </div>
         )
     }
